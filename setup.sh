56220b4052c4f4229fe7c5b2afa1913ee9a236f7
Jakub Kanclerz <jakub.kanclerz@gmail.com>
2020-10-17 11:55:50 +0200

    update setup dir

--- setup.sh
+++ setup.sh
@@ -3,6 +3,14 @@
 ## Cron restart
 #rc-service crond restart
 
+## REPOS
+#REPOS
+echo "http://ftp.icm.edu.pl/pub/Linux/distributions/alpine/v3.10/main" > /etc/apk/repositories
+echo "http://ftp.icm.edu.pl/pub/Linux/distributions/alpine/v3.10/community" >> /etc/apk/repositories
+echo "#http://ftp.icm.edu.pl/pub/Linux/distributions/alpine/edge/main" >> /etc/apk/repositories
+echo "#http://ftp.icm.edu.pl/pub/Linux/distributions/alpine/edge/community" >> /etc/apk/repositories
+echo "#http://ftp.icm.edu.pl/pub/Linux/distributions/alpine/edge/testing" >> /etc/apk/repositories
+
 ##system dependency
 apk --update add cmake gcc g++ linux-headers libffi-dev openssl-dev \
 	openblas openblas-dev \
