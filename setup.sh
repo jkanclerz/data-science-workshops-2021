4c546f96e55c334b35d5ced8c8b86d5e1fc0afdc
Jakub Kanclerz <jakub.kanclerz@gmail.com>
2020-10-17 12:08:18 +0200

    update setup dir

--- setup.sh
+++ setup.sh
@@ -11,6 +11,7 @@ echo "#http://ftp.icm.edu.pl/pub/Linux/distributions/alpine/edge/main" >> /etc/a
 echo "#http://ftp.icm.edu.pl/pub/Linux/distributions/alpine/edge/community" >> /etc/apk/repositories
 echo "#http://ftp.icm.edu.pl/pub/Linux/distributions/alpine/edge/testing" >> /etc/apk/repositories
 
+
 ##system dependency
 apk --update add cmake gcc g++ linux-headers libffi-dev openssl-dev \
 	openblas openblas-dev \
