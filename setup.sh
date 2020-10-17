e04937b6948a2df37208ad30c7992d78c96fe0f3
Jakub Kanclerz <jakub.kanclerz@gmail.com>
2020-10-17 11:50:28 +0200

    update setup dir

--- setup.sh
+++ setup.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env sh
 
+## Cron restart
 rc-service crond restart
 
-## Install python dependency
-pip install -r requirements.txt
+## variables 
+DS_ROOT=`dirname "$0"`
 
-echo "We are ready to go!!!!"
+## Ensure venv exists
+python3.8 -m venv ${DS_ROOT}/.venv
+## Install python dependency
+${DS_ROOT}/.venv/bin/pip install -r ${DS_ROOT}/requirements.txt
+
+echo "We are ready to go!!!!
