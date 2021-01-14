d10e66754cc0d684f0520a5b91b75645d425c7e1
Jakub Kanclerz <jakub.kanclerz@gmail.com>
2021-01-14 16:50:30 +0100

    update setup

--- setup.sh
+++ setup.sh
@@ -18,9 +18,6 @@ apk --update add \
     tar gzip \
 	wget
 
-
-apk --update add libxml2-dev libxslt-dev libffi-dev gcc musl-dev libgcc openssl-dev
-apk --update add  jpeg-dev zlib-dev freetype-dev lcms2-dev openjpeg-dev tiff-dev tk-dev tcl-dev
 ## Docker dep
 apk --update add docker \
 	docker-compose \
